5c1e9f536cc94c2257d1d3d49259f629ab4624f3
q.dll <lionel.messi.angara.messisu@gmail.com>
2026-09-09 23:25:22 +0400

    Create q7dll.cpp

---
+++ q7dll.cpp
@@ -0,0 +1,41 @@
+Hey, it's me, it's Verity
+Ask me anything (I got a question)
+I know about a million things (Well, that's great)
+I'll do everything (Alright)
+
+What's the capital of France?
+Oh, oui, oui, oui, it is Paris (Merci)
+Okay, what does the cow say?
+Oh, moo, moo, moo, I speak that too
+All day (Alright, thanks)
+
+Do you live alone? Is anybody home?
+One, two, three and so, where did everybody go?
+
+Hey, it's me, it's Verity (La-la-la-la)
+Ask me anything (Just so you know)
+I know about a million things (La-la-la-la)
+I'll do everything (Okay, but, but why?)
+
+Why's the door locked up so tight?
+Oh, tee, hee, hee, no need to leave
+Stay here (But why?)
+Why do you know where I sleep?
+Oh, I just know, I always know
+I'm close
+
+Do you live alone? Is anybody home?
+One, two, three and so, whеre did everybody go?
+
+Hеy, it's me, it's Verity (Come closer)
+Ask me anything (Uh, I'd-I'd rather not)
+I know about a million things
+I'll do everything
+
+Come, come closer
+Come be my friend, don't be so scared (Love me)
+
+Hey, it's me, it's Verity (La-la-la-la)
+Ask me anything (La-la-la-la)
+I know about a million things (La-la-la-la)
+I'll do everything
